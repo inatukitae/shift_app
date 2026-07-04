4b5d6827e590e944e5315cdef1ec6296f03eb491
inatukitae <fuvy5753@gmail.com>
2026-07-04 22:49:44 +0900

    11

--- test/system/staffs_test.rb
+++ test/system/staffs_test.rb
@@ -1,40 +1,45 @@
 require "application_system_test_case"
 
 class StaffsTest < ApplicationSystemTestCase
+  include Devise::Test::IntegrationHelpers
+
   setup do
     @staff = staffs(:one)
+    @user = users(:one)
+    login_as @user
   end
 
   test "visiting the index" do
     visit staffs_url
-    assert_selector "h1", text: "Staffs"
+    assert_selector "h1", text: "スタッフ一覧"
   end
 
   test "should create staff" do
     visit staffs_url
-    click_on "New staff"
+    click_on "新規スタッフ登録" # ビューのリンク名に変更
 
-    fill_in "Name", with: @staff.name
-    click_on "Create Staff"
+    fill_in "名前", with: @staff.name # ラベル名「名前」に変更
+    select "薬剤師", from: "職種"    # セレクトボックス用に追加
+    click_on "保存する"             # フォームのボタン名に変更
 
-    assert_text "Staff was successfully created"
-    click_on "Back"
+    assert_text "Staff was successfully created" # ここはコントローラーのメッセージに合わせてください
+    click_on "スタッフ一覧に戻る"
   end
 
   test "should update Staff" do
     visit staff_url(@staff)
-    click_on "Edit this staff", match: :first
+    click_on "編集する" # ビューのリンク名に変更
 
-    fill_in "Name", with: @staff.name
-    click_on "Update Staff"
+    fill_in "名前", with: @staff.name
+    click_on "保存する" # フォームのボタン名に変更
 
     assert_text "Staff was successfully updated"
-    click_on "Back"
+    click_on "スタッフ一覧に戻る"
   end
 
   test "should destroy Staff" do
     visit staff_url(@staff)
-    click_on "Destroy this staff", match: :first
+    click_on "削除する" # ビューのリンク名に変更
 
     assert_text "Staff was successfully destroyed"
   end
