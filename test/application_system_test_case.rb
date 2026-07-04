1013b33352d8719ca03af2d096e6cb6c672cbf76
inatukitae <fuvy5753@gmail.com>
2026-07-04 23:28:48 +0900

    13

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,10 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+  # ヘッドレスChromeを指定し、Docker環境に必要なフラグを追加します
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400] do |driver_options|
+    driver_options.add_argument('--no-sandbox')
+    driver_options.add_argument('--disable-dev-shm-usage')
+    driver_options.add_argument('--headless=new')
+  end
 end
