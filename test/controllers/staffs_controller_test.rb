2b15dc9db813a1d9002ca6861042784fa23206c1
inatukitae <fuvy5753@gmail.com>
2026-07-04 22:40:13 +0900

    10

--- test/controllers/staffs_controller_test.rb
+++ test/controllers/staffs_controller_test.rb
@@ -1,52 +1,44 @@
-require "test_helper"
+require "application_system_test_case"
 
-class StaffsControllerTest < ActionDispatch::IntegrationTest
-  include Devise::Test::IntegrationHelpers # Deviseのテストヘルパーを有効化
-
+class StaffsTest < ApplicationSystemTestCase
   setup do
     @staff = staffs(:one)
-    @user = users(:one) # フィクスチャからログイン用のユーザーを取得
-    sign_in @user        # 各テストの前にログインさせる
+    # ログインが必要な場合はここに追記
+    # sign_in users(:one)
   end
 
-  test "should get index" do
-    get staffs_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_staff_url
-    assert_response :success
+  test "visiting the index" do
+    visit staffs_url
+    assert_selector "h1", text: "スタッフ一覧"
   end
 
   test "should create staff" do
-    assert_difference("Staff.count") do
-      post staffs_url, params: { staff: { name: @staff.name, job_type: @staff.job_type } }
-    end
+    visit staffs_url
+    click_on "新規スタッフ登録"
 
-    assert_redirected_to staff_url(Staff.last)
+    fill_in "名前", with: @staff.name
+    select "薬剤師", from: "職種" # selectボックスの操作
+    click_on "保存する" # フォーム内のボタン名に修正
+
+    assert_text "Staff was successfully created"
+    click_on "スタッフ一覧に戻る"
   end
 
-  test "should show staff" do
-    get staff_url(@staff)
-    assert_response :success
+  test "should update Staff" do
+    visit staff_url(@staff)
+    click_on "編集する"
+
+    fill_in "名前", with: @staff.name
+    click_on "保存する" # フォーム内のボタン名に修正
+
+    assert_text "Staff was successfully updated"
+    click_on "スタッフ一覧に戻る"
   end
 
-  test "should get edit" do
-    get edit_staff_url(@staff)
-    assert_response :success
-  end
+  test "should destroy Staff" do
+    visit staff_url(@staff)
+    click_on "削除する"
 
-  test "should update staff" do
-    patch staff_url(@staff), params: { staff: { name: @staff.name } }
-    assert_redirected_to staff_url(@staff)
-  end
-
-  test "should destroy staff" do
-    assert_difference("Staff.count", -1) do
-      delete staff_url(@staff)
-    end
-
-    assert_redirected_to staffs_url
+    assert_text "Staff was successfully destroyed"
   end
 end
